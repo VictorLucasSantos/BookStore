9ba8d36339b229bc7f81b96f45b855faa0daff58
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-06-23 19:01:36 -0400

    Corrige uso do debbugtoolbar

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+IS_TESTING = 'test' in sys.argv
 
 # Application definition
 
@@ -53,9 +55,11 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware"
 ]
+
+if DEBUG and not IS_TESTING:
+    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
 
 ROOT_URLCONF = "BookStore.urls"
 
